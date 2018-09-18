98164441be16d86aa89a2f0b08a2fe429d07f644
jmruth2 <jmruth2@illinois.edu>
2018-09-18 12:15:08 -0500

    Initial Project Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Here");
+        String url = "https://www.bls.gov/tus/charts/chart9.txt";
+        System.out.println(WebScraper.urlToString(url));
+        System.out.println("Word count: " + WebScraper.wordCount(WebScraper.urlToString(url)));
+        System.out.println("Unique word count: " + WebScraper.uniqueWordCount(WebScraper.urlToString(url), "data"));
     }
 }
--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,6 +1,44 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class WebScraper {
-    public static void main(String[] args) {
-        System.out.println("Ran");
-                
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        int wordCount = 0;
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    public static int wordCount(String text) {
+        int wordCount = 0;
+        Scanner wordScanner = new Scanner(text);
+        while (wordScanner.hasNext()) {
+            wordCount++;
+            wordScanner.next();
+        }
+        return wordCount;
+    }
+
+    public static int uniqueWordCount(String text, String word) {
+        int wordCount = 0;
+        Scanner wordScanner = new Scanner(text);
+        while (wordScanner.hasNext()) {
+            if (wordScanner.next().equalsIgnoreCase(word)) {
+                wordCount++;
+            }
+        }
+        return wordCount;
     }
 }
